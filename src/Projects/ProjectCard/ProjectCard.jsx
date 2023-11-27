624dbe4ac87aeab859b780ef9242a598a6649f9b
Abdullah Tassawar <abdullahtassawar007@gmail.com>
2023-11-27 12:10:43 +0500

    add: initial files

--- src/Projects/ProjectCard/ProjectCard.jsx
+++ src/Projects/ProjectCard/ProjectCard.jsx
@@ -8,13 +8,13 @@ import {
   TechCard,
   BtnGroup,
 } from "./ProjectCardElements";
-import ScrollAnimation from "react-animate-on-scroll";
+
 import { ProjectList } from "../../ProjectData";
 function ProjectCard() {
   return (
     <>
       {ProjectList.map((list, index) => (
-        <ScrollAnimation animateIn="fadeInLeft" key={index}>
+
           <Card>
             <CardLeft>
               <img src={list.img} alt={list.name} />
@@ -51,7 +51,7 @@ function ProjectCard() {
               </BtnGroup>
             </CardRight>
           </Card>
-        </ScrollAnimation>
+ 
       ))}
     </>
   );
